cf30135abaf73cf3cefc7514790c2ed7139a2b6c
Alba Mendez <me@alba.sh>
2024-10-16 23:24:20 +0200

    sysreg: refactor to prepare for next commit

--- proxyclient/m1n1/sysreg.py
+++ proxyclient/m1n1/sysreg.py
@@ -7,27 +7,34 @@ from .utils import Register, Register64, Register32
 __all__ = ["sysreg_fwd", "sysreg_rev", "SysReg", "SysRegEnc"]
 
 def _load_registers():
-    global sysreg_fwd, sysop_fwd
+    global sysreg_fwd, sysop_fwd, sysop_fwd_id
 
     sysreg_fwd = {}
     sysop_fwd = {}
+    sysop_fwd_id = {}
     for fname in ["arm_regs.json", "apple_regs.json"]:
         data = json.loads(resources.read_text(__package__, fname, encoding="utf-8"))
 
         for reg in data:
+            add_fwd = False
+            enc = tuple(reg["enc"])
+
             if "accessors" in reg:
                 for acc in reg["accessors"]:
                     if acc in ("MRS", "MSR"):
-                        sysreg_fwd[reg["name"]] = tuple(reg["enc"])
+                        add_fwd = True
                     else:
-                        sysop_fwd[acc + " " + reg["name"]] = tuple(reg["enc"])
+                        sysop_fwd[key := acc + " " + reg["name"]] = enc
+                        sysop_fwd_id[key := key.replace(" ", "_")] = enc
             else:
-                sysreg_fwd[reg["name"]] = tuple(reg["enc"])
+                add_fwd = True
+
+            if add_fwd:
+                sysreg_fwd[reg["name"]] = enc
 
 _load_registers()
 sysreg_rev = {v: k for k, v in sysreg_fwd.items()}
 sysop_rev = {v: k for k, v in sysop_fwd.items()}
-sysop_fwd_id = {k.replace(" ", "_"): v for k,v in sysop_fwd.items()}
 
 globals().update(sysreg_fwd)
 __all__.extend(sysreg_fwd.keys())
